1de6e2151370edd0cb6410043c2644f744e0649d
devsbrana <sbranaww@gmail.com>
2026-04-15 21:49:31 -0300

    Adicionando Icons no drawerLabel

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,27 @@
 import { Drawer } from 'expo-router/drawer';
- 
+import { Ionicons } from '@expo/vector-icons'; // 👈 Importa os ícones
+
 export default function Layout() {
   return (
     <Drawer>
       <Drawer.Screen
         name="cadastro" 
         options={{
-          title: 'cadastro',
+          title: 'Cadastro',
+          drawerLabel: 'Cadastro',
+          drawerIcon: ({ color, size }) => ( 
+            <Ionicons name="person-add-outline" size={size} color={color} />
+          ),
         }}
       />
       <Drawer.Screen
         name="user" 
         options={{
-          title: 'user',
+          title: 'User',
+          drawerLabel: 'User',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
         }}
       />
     </Drawer>
